dc0131c1ee35d3b256985bfc2418b96ab72026f3
FajarHosting <82355656+FajarHosting@users.noreply.github.com>
2025-04-22 21:22:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-// ===== KONFIGURASI API DI SINI =====
-const IMAGE_API_URL = "https://api.deepai.org/api/torch-srgan";
-const IMAGE_API_KEY = "https://api.deepai.org/api/torch-srgan";
-const VIDEO_API_BASE = "https://api.vevioz.com/api/button/mp4?url=";
+yu// ===== KONFIGURASI API DI SINI =====
+const IMAGE_API_URL = "https://www.tikwm.com/api/";
+const IMAGE_API_KEY = "https://www.tikwm.com/api/";
+const VIDEO_API_BASE = "https://www.tikwm.com/api/";
 // ====================================
