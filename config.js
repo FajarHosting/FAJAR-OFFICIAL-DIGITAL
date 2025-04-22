18e2fa44207f28c663babc6bd4c26b5e18411d99
FajarHosting <82355656+FajarHosting@users.noreply.github.com>
2025-04-22 21:43:50 +0800

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-yu// ===== KONFIGURASI API DI SINI =====
-const IMAGE_API_URL = "https://www.tikwm.com/api/";
-const IMAGE_API_KEY = "https://www.tikwm.com/api/";
-const VIDEO_API_BASE = "https://www.tikwm.com/api/";
-// ====================================
